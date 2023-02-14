8f470633138d66ec07cfbb4218461701980c4d84
Gabriel Reis Miranda <grmiranda97@gmail.com>
2023-02-14 17:26:53 -0300

    🐛 Main Redirect

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,7 @@ import Quote from '@/views/Quote.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/', redirect: '/auth/login' },
     {
       path: '/auth',
       component: BlancLayout,
